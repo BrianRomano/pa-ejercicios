6d318353848b4118a13de3100c7a40b14a8cb5fd
Brian Romano <brianromanook@gmail.com>
2021-04-06 19:48:06 -0300

    Ejercicio 1

---
+++ 01-ejercicio/src/Account.java
@@ -0,0 +1,24 @@
+public class Account {
+
+    // Variable
+    private double balance;
+
+    // Constructor
+    public Account(double initBalance) {
+        balance = initBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amt) {
+        balance += amt;
+    }
+
+    public void withdraw(double amt) {
+        if (amt <= balance) {
+            balance -= amt;
+        }
+    }
+}
